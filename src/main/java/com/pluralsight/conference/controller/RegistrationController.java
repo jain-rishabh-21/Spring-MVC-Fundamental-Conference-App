25f41de0e681c6ea2442cb31f7bcdd87aaf12998
kackerzzz <rishabhee21@gmail.com>
2020-05-17 08:18:21 +0530

    Added a validator for name property

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -2,10 +2,12 @@ package com.pluralsight.conference.controller;
 
 import com.pluralsight.conference.model.Registration;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -15,8 +17,15 @@ public class RegistrationController {
     public String getRegistration(@ModelAttribute ("registration")Registration registration){
         return "registration";
     }
+
     @PostMapping(value = "registration")
-    public String addRegistration(@ModelAttribute ("registration")Registration registration){
+    public String addRegistration(@Valid @ModelAttribute ("registration")Registration registration, BindingResult result){
+
+        if(result.hasErrors()){
+            System.out.println("Errors!!!");
+            return "registration";
+        }
+
         System.out.println("Registration: " + registration.getName());
         return "redirect:registration";
     }
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -12,10 +12,24 @@
 <html>
 <head>
     <title>Registration</title>
+    <style>
+        .error {
+            color: #ff0000;
+        }
+
+        .errorblock {
+            color: #000;
+            background-color: red;
+            border: 3px solid red;
+            padding: 8px;
+            margin: 16px;
+        }
+    </style>
 </head>
 <body>
 <h1>Registration</h1>
 <form:form modelAttribute="registration">
+    <form:errors path="*" cssClass="errorblock" element="div" />
     <table>
         <tr>
             <td>
@@ -25,9 +39,12 @@
             <td>
                 <form:input path="name"/>
             </td>
+            <td>
+                <form:errors path="name" cssClass="error" />
+            </td>
         </tr>
         <tr>
-            <td colspan="2">
+            <td colspan="3">
                 <input type="submit" value="Add Registration">
             </td>
         </tr>
