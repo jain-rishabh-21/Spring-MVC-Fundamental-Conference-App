f6ca87637ea213aea61af5ce23ac1e97af996ac2
kackerzzz <rishabhee21@gmail.com>
2020-05-17 05:39:54 +0530

    First Commit, Spring MVC Fundamental ConferenceApplication with Tomcat Added as Application Server

---
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -0,0 +1,16 @@
+package com.pluralsight.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping(value = "registration")
+    public String getRegistration(Map<String, Object> model){
+
+        return "registration";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kackerzzz
+  Date: 17/05/20
+  Time: 5:31 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+</body>
+</html>
