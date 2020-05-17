747bd25f9862c01405722f44cd999b18d37d3900
kackerzzz <rishabhee21@gmail.com>
2020-05-17 06:01:11 +0530

    Added the post method and Registration Page

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -1,7 +1,10 @@
 package com.pluralsight.conference.controller;
 
+import com.pluralsight.conference.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -9,8 +12,12 @@ import java.util.Map;
 public class RegistrationController {
 
     @GetMapping(value = "registration")
-    public String getRegistration(Map<String, Object> model){
-
+    public String getRegistration(@ModelAttribute ("registration")Registration registration){
+        return "registration";
+    }
+    @PostMapping(value = "registration")
+    public String addRegistration(@ModelAttribute ("registration")Registration registration){
+        System.out.println("Registration: " + registration.getName());
         return "registration";
     }
 }
