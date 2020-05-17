a4a071bdb5ac52dacbcbe4dd60f1c17abbf476cd
kackerzzz <rishabhee21@gmail.com>
2020-05-17 06:28:23 +0530

    Created Handler for static Files

--- src/main/java/com/pluralsight/conference/model/ConferenceConfig.java
+++ src/main/java/com/pluralsight/conference/model/ConferenceConfig.java
@@ -3,13 +3,21 @@ package com.pluralsight.conference.model;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
 
     @Bean
-    public ViewResolver viewResolver(){
+    public ViewResolver viewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
         bean.setPrefix("/WEB-INF/jsp/");
         bean.setSuffix(".jsp");
